d5e5cc9ec5f266a4c42e37251e2c3c3b1d8b7e0b
João <joaovictorleal080415@gmail.com>
2022-11-11 01:03:23 -0300

    testando

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,7 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+<<<<<<< HEAD
 export function Example(): JSX.Element { 
 
 	return <h1>Olá, mundo </h1>;
 
 }
+=======
+export function Example(): JSX.Element {
+  return <h1>Olá, mundo </h1>;
+}
+>>>>>>> 4e33214 (testando)
