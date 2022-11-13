f5c9cce0313a6573c3eb667b3bf16a2d1a86c05e
João <joaovictorleal080415@gmail.com>
2022-11-13 00:21:55 -0300

    chore: new files whith correction

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,17 @@ import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+<<<<<<< HEAD
 >>>>>>> 4e33214 (testando)
+=======
+import { ChakraProvider } from '@chakra-ui/react';
+>>>>>>> 0bd2c80 (chore: create initial setup)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 <<<<<<< HEAD
 )
