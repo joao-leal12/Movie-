dc1154d6fd27945e1471e9e0c1c79ca93d61b52a
João <joaovictorleal080415@gmail.com>
2022-11-20 00:47:31 -0300

    refactor: remove conflicts

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,13 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-<<<<<<< HEAD
-export function Example(): JSX.Element { 
-
-	return <h1>Olá, mundo </h1>;
-
-}
-=======
 export function Example(): JSX.Element {
   return <h1>Olá, mundo </h1>;
 }
->>>>>>> 4e33214 (testando)
