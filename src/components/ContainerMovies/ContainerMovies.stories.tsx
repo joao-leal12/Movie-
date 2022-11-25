9e7e03fc3aaec4988973e49837ea400cf400d5c4
João <joaovictorleal080415@gmail.com>
2022-11-24 23:39:55 -0300

    chore: i disabled the rule of typescript-consistent-type-assertions

--- src/components/ContainerMovies/ContainerMovies.stories.tsx
+++ src/components/ContainerMovies/ContainerMovies.stories.tsx
@@ -1,6 +1,6 @@
 import { ContainerMovies } from '.';
 import { Meta, StoryObj } from '@storybook/react';
-// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+
 export default {
   title: 'components/ContainerMovies',
   component: ContainerMovies,
