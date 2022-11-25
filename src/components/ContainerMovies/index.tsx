efe7097ac519f6046f1cc75c5fc55358c99829a5
João <joaovictorleal080415@gmail.com>
2022-11-24 23:39:55 -0300

    chore: i disabled the rule of typescript-consistent-type-assertions

--- src/components/ContainerMovies/index.tsx
+++ src/components/ContainerMovies/index.tsx
@@ -2,7 +2,7 @@ import { chakra } from '@chakra-ui/react';
 import { MoviesList } from '../MoviesList';
 export const ContainerMovies = (): JSX.Element => {
   return (
-    <chakra.section flex="2" mt="2rem">
+    <chakra.section flex="2" mt="2rem ">
       <MoviesList />
     </chakra.section>
   );
