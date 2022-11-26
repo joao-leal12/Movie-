da84ee53e119308b282a5f7b54f6f70ff98dc8bd
João <joaovictorleal080415@gmail.com>
2022-11-26 01:49:18 -0300

    feat: menu component initialization

---
+++ src/components/SidebarItems/index.tsx
@@ -0,0 +1,9 @@
+import { SidebarLinks } from '../SidebarLinks';
+import { Flex } from '@chakra-ui/react';
+export const SidebarItems = () => {
+  return (
+    <Flex>
+      <SidebarLinks />
+    </Flex>
+  );
+};
---
+++ src/utils/linksSIdebar.ts
@@ -0,0 +1,6 @@
+import { Sword, MaskSad } from 'phosphor-react';
+
+export const Links = [
+  { genre: 'Aventura', icon: Sword, id: 1 },
+  { genre: 'Drama', icon: MaskSad, id: 2 },
+];
