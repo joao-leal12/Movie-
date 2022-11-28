b1d1ac014ed00c43d3fe0c27419d979638b43215
João <joaovictorleal080415@gmail.com>
2022-11-28 16:40:24 -0300

    feat: add Storybook in sidebarLinks with styles

--- src/components/SidebarLinks/SidebarLinks.stories.tsx
+++ src/components/SidebarLinks/SidebarLinks.stories.tsx
@@ -2,7 +2,8 @@ import { SidebarLinks } from '.';
 import { Meta, StoryObj } from '@storybook/react';
 import { BrowserRouter } from 'react-router-dom';
 import { SidebarWrapperContainer } from '../SidebarWrapperContainer';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import { ThemeConfig } from '../../Styles/theme';
 export default {
   title: 'components/SidebarLinks',
   component: SidebarLinks,
@@ -10,7 +11,9 @@ export default {
     (Story) => {
       return (
         <BrowserRouter>
-          <SidebarWrapperContainer>{Story()}</SidebarWrapperContainer>
+          <ChakraProvider theme={ThemeConfig}>
+            <SidebarWrapperContainer>{Story()}</SidebarWrapperContainer>
+          </ChakraProvider>
         </BrowserRouter>
       );
     },
