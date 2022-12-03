ff8c52c74c51b4471f058c183b166683d10e5207
João <joaovictorleal080415@gmail.com>
2022-12-02 23:46:04 -0300

    feat: add Storybook in Sidebar Component

---
+++ src/components/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,21 @@
+import { Sidebar } from '.';
+import { Meta, StoryObj } from '@storybook/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeConfig } from '../../Styles/theme';
+
+export default {
+  title: 'SidebarComponents/Sidebar',
+  component: Sidebar,
+  decorators: [
+    (Story) => {
+      return (
+        <BrowserRouter>
+          <ChakraProvider theme={ThemeConfig}>{Story()}</ChakraProvider>
+        </BrowserRouter>
+      );
+    },
+  ],
+} as Meta;
+
+export const Default: StoryObj = {};
