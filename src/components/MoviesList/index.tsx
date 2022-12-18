2130c71a9d2c9643d1dc9919a3f2fdcff91cce4f
João <joaovictorleal080415@gmail.com>
2022-12-18 02:55:27 -0300

    feat: adjust scroll infinite

--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -1,17 +1,18 @@
 import { chakra } from '@chakra-ui/react';
 import { MovieCard } from '../MovieCard';
 import { GET_MOVIES } from '../../utils/API/API_ROUTES';
-import { useEffect, useState, useRef, Fragment } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { UseFetch } from '../../hooks/UseFetch';
 import { IMovieData } from '../../types/ApiReturn';
-
+import { Loading } from '../helpers/loading';
+type IWait = boolean;
 export const MoviesList = () => {
   const { request, loading } = UseFetch();
   const [data, setData] = useState<IMovieData[] | null>(null);
   const [limitRender, SetLimitRender] = useState(6);
   const [count, setCount] = useState(1);
-  const [infinite, setInfinite] = useState(true);
-
+  const [infinite, setInfinite] = useState(false);
+  const [wait, setWait] = useState<IWait>(false);
   const observer = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -24,6 +25,7 @@ export const MoviesList = () => {
 
     void getMovieData();
   }, []);
+
   useEffect(() => {
     if (data !== null && limitRender >= data.length) {
       setInfinite(false);
@@ -31,17 +33,26 @@ export const MoviesList = () => {
   }, [limitRender, data, infinite]);
   useEffect(() => {
     const intersectOb = new IntersectionObserver((entries) => {
-      // SetLimitRender((limitRender) => limitRender * count);
       if (data != null) {
         const limit = Math.floor(data.length / 6);
         const rest = data.length % 6;
 
+        setInfinite(true);
+
         if (entries.some((entry) => entry.isIntersecting)) {
-          if (count < limit && infinite) {
-            setCount((Count) => Count + 1);
-            SetLimitRender((limitRender) => limitRender + 6);
-          } else if (infinite) {
-            SetLimitRender((limitRender) => limitRender + rest);
+          if (count < limit && infinite && !wait) {
+            setWait(true);
+            setTimeout(() => {
+              setWait(false);
+              setCount((Count) => Count + 1);
+              SetLimitRender((limitRender) => limitRender + 6);
+            }, 500);
+          } else if (infinite && count >= limit) {
+            setWait(true);
+            setTimeout(() => {
+              setWait(false);
+              SetLimitRender((limitRender) => limitRender + rest);
+            }, 500);
           }
         }
       }
@@ -49,9 +60,9 @@ export const MoviesList = () => {
     if (observer.current != null) intersectOb.observe(observer.current);
 
     return () => intersectOb.disconnect();
-  }, [limitRender, count, data, infinite]);
+  }, [limitRender, count, data, infinite, wait]);
 
-  if (loading) return <p style={{ color: 'white' }}>Carregando caralho....</p>;
+  if (loading) return <p style={{ color: 'white' }}>Carregando....</p>;
   if (data != null)
     return (
       <chakra.ul
@@ -64,7 +75,7 @@ export const MoviesList = () => {
           index < limitRender ? (
             <MovieCard dataMovie={movie} key={movie.id + index} />
           ) : (
-            <Fragment key={movie.id + index}></Fragment>
+            <Loading key={movie.id + index} />
           )
         )}
         <div ref={observer} style={{ width: '100%', height: '90px' }}></div>
